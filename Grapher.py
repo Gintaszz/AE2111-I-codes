070b52fb41b8d2fb512bda85bf1a4fd28c8240ae
Gintaszz <brolis.gintux@gmail.com>
2023-12-11 14:25:00 +0100

    Something was indeed done

--- Grapher.py
+++ Grapher.py
@@ -21,7 +21,9 @@ for ax in [ax1,ax2,ax3]:
     # hides borders
     ax.axis[direction].set_visible(False)
   ax.grid()
+  ax.set_yscale('log')
 x = np.linspace(bounds[0], bounds[1], 150)
+
 ax1.plot(x,[sigmaY/stress(i,0) for i in x if True])
 ax2.plot(x,[sigmaY/stress(i,1) for i in x if True])
 ax3.plot(x,[sigmaY/stress(i,2) for i in x if True])
--- buckling.py
+++ buckling.py
@@ -1,11 +1,23 @@
 # Maximum allowable stress as a function of span
 # Web, Skin, Column, Global Buckling
 import numpy as np
-from data import design, C_R, topweb, bottomweb, centroid
+from data import design, C_R, topweb, bottomweb, centroid, c, SEMISPAN, E
+import matplotlib.pyplot as plt
 
-def skinmaxstress(y):
+def skinmaxstress(y, designindex):
     chord = c(y)
-    
-    k_c = (frontsparlength + backsparlength)/2 * spartdist # should be constant
+    frontsparlength = design['front spar to root chord'][designindex]/C_R * chord
+    backsparlength = design['back spar to root chord'][designindex]/C_R * chord
+    spardist = design['spar distance x'][designindex] * chord
+
+    k_c = 7
+    t = design['t web'][designindex]
+    b = (frontsparlength + backsparlength)/2
+    poission = 1/3
     sigma = np.pi**2 * k_c * E * (t/b)**2 / (12(1-poission**2))
-    return
+
+    return sigma
+
+plotlist = [skinmaxstress(y, 0) for y in np.arange(0, SEMISPAN, 0.1)]
+plt.plot(np.arange(0, SEMISPAN, 0.1), plotlist)
+plt.show()

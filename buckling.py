0ee9643fb406fe4525b55a634d14c42039bd04b9
Gintaszz <brolis.gintux@gmail.com>
2023-12-11 14:09:55 +0100

    comit for shear_pls

--- buckling.py
+++ buckling.py
@@ -1,2 +1,11 @@
 # Maximum allowable stress as a function of span
 # Web, Skin, Column, Global Buckling
+import numpy as np
+from data import design, C_R, topweb, bottomweb, centroid
+
+def skinmaxstress(y):
+    chord = c(y)
+    
+    k_c = (frontsparlength + backsparlength)/2 * spartdist # should be constant
+    sigma = np.pi**2 * k_c * E * (t/b)**2 / (12(1-poission**2))
+    return
--- stress.py
+++ stress.py
@@ -1,4 +1,4 @@
-from zenvmer import moment_pls, torque_pls, bounds
+from zenvmer import shear_pls, moment_pls, torque_pls, bounds
 from main import I, c
 from data import design, C_R, topweb, bottomweb, centroid
 import numpy as np
@@ -9,10 +9,8 @@ def stress(y, designindex):
     chord = c(y)
 
     #bending stress
-    print(abs(topweb(0, chord, designindex)-centroid(chord, designindex)), abs(bottomweb(0, chord, designindex)-centroid(chord, designindex)))
     max_z = max(abs(topweb(0, chord, designindex)-centroid(chord, designindex)), abs(bottomweb(0, chord, designindex)-centroid(chord, designindex)))
     normal_stress = np.abs(moment_pls(y) * max_z / I(y, designindex)) #Absoluite value for now !!!!!!!!!!!!!!!! must be changed
-    print(normal_stress)
     #torque shear stress
     # \tau = q/t = T/(2tA_m)
     #Area of trapezium
@@ -27,5 +25,3 @@ def stress(y, designindex):
     equ_normal = mohrs_radius + normal_stress/2
 
     return equ_normal
-
-print(stress(0, 0))
